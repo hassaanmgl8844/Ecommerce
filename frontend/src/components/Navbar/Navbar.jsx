5fbe8e89acbab883069bac9dcd1714ff239492a9
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-12-26 18:51:54 +0500

    Working on Navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,19 +1,61 @@
+const MenuLinks = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/#",
+  },
+  {
+    id: 1,
+    name: "Shop",
+    link: "/#shop",
+  },
+  {
+    id: 1,
+    name: "About",
+    link: "/#about",
+  },
+  {
+    id: 4,
+    name: "Blogs",
+    link: "/#blog",
+  },
+];
 
 const Navbar = () => {
   return (
-    <div>
-        <div>
-            <div className="container">
-                {/* Logo and Links Section */}
-                    <div>
-                        <a href="#"></a>
-                    </div>
-
-                {/* Navbar Right Section */}
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
+      <div className="py-4">
+        <div className="container">
+          {/* Logo and Links Section */}
+          <div className="flex items-center gap-4">
+            <a
+              href="#"
+              className="text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl"
+            >
+              HHEshop
+            </a>
+            {/* Menu Items */}
+            <div className="hidden lg:block">
+              <ul className="flex items-center gap-4">
+                {MenuLinks.map((data, index) => (
+                  <li key={index}>
+                    <a
+                      href={data.link}
+                      className="inline-block px-4 font-semibold text-gray-500 hover:text-black dark:hover:text-white duration-200"
+                    >
+                      {data.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
             </div>
-        </div>
-    </div>
-  )
-}
+          </div>
 
-export default Navbar
+          {/* Navbar Right Section */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
