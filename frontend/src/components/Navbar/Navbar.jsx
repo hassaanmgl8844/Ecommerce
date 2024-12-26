3a94945d0abfd9d724dc926930aab0c26b252fc8
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-12-26 18:23:26 +0500

    Starting Navbar Section

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            <div className="container">
+                {/* Logo and Links Section */}
+                    <div>
+                        <a href="#"></a>
+                    </div>
+
+                {/* Navbar Right Section */}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
